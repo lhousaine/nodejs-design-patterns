cd8c21c5884a651de407bb9ece8676868e0f8bf7
lhousaine <l.ouarhou.ensa@.com>
2022-03-27 21:03:13 +0000

    add stream demonstrating performance example

---
+++ chapter6/platform/gzip-stream.js
@@ -0,0 +1,8 @@
+// gzip-stream.js
+import { createReadStream, createWriteStream } from 'fs'
+import { createGzip } from 'zlib'
+const filename = process.argv[2]
+createReadStream(filename)
+ .pipe(createGzip())
+ .pipe(createWriteStream(`${filename}.gz`))
+ .on('finish', () => console.log('File successfully compressed'))
