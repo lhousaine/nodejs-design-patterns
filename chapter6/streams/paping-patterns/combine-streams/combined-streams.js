272c23d366fc1ab48dd0ffd2c68a04197334608d
lhousaine <l.ouarhou.ensa@.com>
2022-03-27 21:09:33 +0000

    add paping patterns example

---
+++ chapter6/streams/paping-patterns/combine-streams/combined-streams.js
@@ -0,0 +1,29 @@
+import { createGzip, createGunzip } from 'zlib';
+import {
+    createCipheriv,
+    createDecipheriv,
+    scryptSync
+} from 'crypto';
+import pumpify from 'pumpify'; // A library used to create a combined streams
+
+function createKey(password) {
+    return scryptSync(password, 'salt', 24);
+}
+
+export function createCompressAndEncrypt(password, iv) {
+    const key = createKey(password);
+    const combinedStream = pumpify(
+        createGzip(),
+        createCipheriv('aes192', key, iv)
+    );
+    combinedStream.iv = iv;
+    return combinedStream;
+}
+
+export function createDecryptAndDecompress(password, iv) {
+    const key = createKey(password);
+    return pumpify(
+        createDecipheriv('aes192', key, iv),
+        createGunzip()
+    )
+}
---
+++ chapter6/streams/paping-patterns/combine-streams/index.js
@@ -0,0 +1,31 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { Transform, pipeline } from 'stream';
+import { strict as assert } from 'assert';
+
+const streamA = createReadStream('package.json');
+
+const streamB = new Transform({
+    transform(chunk, enc, done) {
+        this.push(chunk.toString().toUpperCase())
+        done()
+    }
+});
+
+const streamC = createWriteStream('package-uppercase.json');
+
+const pipelineReturn = pipeline(
+    streamA,
+    streamB,
+    streamC,
+    (err) => {
+        // handle errors here
+        if(err){
+         return console.log(` the error was throwing : ${err}`);
+        }
+        console.log(`...end`);
+   }
+);
+
+assert.strictEqual(streamC, pipelineReturn); // valid
+const pipeReturn = streamA.pipe(streamB).pipe(streamC)
+assert.strictEqual(streamC, pipeReturn) // valid
