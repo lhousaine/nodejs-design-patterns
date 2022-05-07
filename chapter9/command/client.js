c93f88cea4fe3f80bd46bc126fee5c00a052fbce
lhousaine <l.ouarhou.ensa@gmail.com>
2022-05-07 22:45:38 +0000

    create chapter9 command example

---
+++ chapter9/command/client.js
@@ -0,0 +1,16 @@
+import { createPostStatusCmd } from './createPostStatusCmd.js';
+import { statusUpdateService } from './statusUpdateService.js';
+
+import { Invoker } from './invoker.js';
+
+const invoker = new Invoker();
+
+const command = createPostStatusCmd(statusUpdateService, 'HI!');
+
+invoker.run(command);
+
+invoker.undo();
+
+invoker.delay(command, 1000 * 3);
+
+invoker.runRemotely(command);
