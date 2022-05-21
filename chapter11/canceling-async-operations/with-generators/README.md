f9c859b0629a3f54316176f8672574ce31d0ea4e
lhousaine <l.ouarhou.ensa@gmail.com>
2022-05-21 13:12:29 +0100

    feat(chapter11): create generator implementation for async operations cancel

---
+++ chapter11/canceling-async-operations/with-generators/README.md
@@ -0,0 +1,5 @@
+# CANCEL ASYNCHRONOUS OPERATIONS WITH GENERATTORS: 
+For use in production, most of the time, we can rely on a widely
+used package from the Node.js ecosystem such as caf (the
+acronym means Cancelable Async Flows), which you can find at
+nodejsdp.link/caf.
---
+++ chapter11/canceling-async-operations/with-generators/createAsyncCancelable.js
@@ -0,0 +1,35 @@
+import { CancelError } from "./cancelError.js";
+
+export function createAsyncCancelable(generatorFunction) {
+  return function asyncCancelable(...args) {
+    const generatorObject = generatorFunction(...args);
+    let cancelRequested = false;
+
+    function cancel() {
+      cancelRequested = true;
+    }
+    // represent the eventual resolution (or rejection) of the asynchronous operation.
+    const promise = new Promise((resolve, reject) => {
+      async function nextStep(prevResult) {
+        if (cancelRequested) {
+          return reject(new CancelError());
+        }
+
+        if (prevResult.done) {
+          return resolve(prevResult.value);
+        }
+        try {
+          nextStep(generatorObject.next(await prevResult.value));
+        } catch (err) {
+          try {
+            nextStep(generatorObject.throw(err));
+          } catch (err2) {
+            reject(err2);
+          }
+        }
+      }
+      nextStep({});
+    });
+    return { promise, cancel };
+  };
+}
