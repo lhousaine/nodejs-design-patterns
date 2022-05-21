f486256e79b2bb684e45922aa8e5af940d927cd5
lhousaine <l.ouarhou.ensa@gmail.com>
2022-05-21 13:12:00 +0100

    feat(chapter11): create wrapper implementation for async operations cancel

---
+++ chapter11/canceling-async-operations/with-wrapper/cancelWrapper.js
@@ -0,0 +1,15 @@
+import { CancelError } from "./cancelError.js";
+
+export function createCancelWrapper() {
+  let cancelRequested = false;
+  function cancel() {
+    cancelRequested = true;
+  }
+  function cancelWrapper(func, ...args) {
+    if (cancelRequested) {
+      return Promise.reject(new CancelError());
+    }
+    return func(...args);
+  }
+  return { cancelWrapper, cancel };
+}
