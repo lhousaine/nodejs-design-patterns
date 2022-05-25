642aa514c283ac0c6db653654911aba623c30080
lhousaine <l.ouarhou.ensa@gmail.com>
2022-05-25 21:19:30 +0100

    feat(chapter12):simulate zero downtime restart

--- chapter12/simple-http-server/app.js
+++ chapter12/simple-http-server/app.js
@@ -1,18 +1,22 @@
 import { createServer } from "http";
 import { cpus } from "os";
 import cluster from "cluster";
+import { once } from "events";
 
 if (cluster.isMaster) {
   // fork()
   const availableCpus = cpus();
   console.log(`Clustering to ${availableCpus.length} processes`);
   availableCpus.forEach(() => cluster.fork());
-  cluster.on("exit", (worker, code) => {
-    if (code !== 0 && !worker.exitedAfterDisconnect) {
-      console.log(
-        `Worker ${worker.process.pid} crashed. ` + "Starting a new worker"
-      );
-      cluster.fork();
+  process.on("SIGUSR2", async () => {
+    const workers = Object.values(cluster.workers);
+    for (const worker of workers) {
+      console.log(`Stopping worker: ${worker.process.pid}`);
+      worker.disconnect();
+      await once(worker, "exit");
+      if (!worker.exitedAfterDisconnect) continue;
+      const newWorker = cluster.fork();
+      await once(newWorker, "listening");
     }
   });
 } else {
@@ -26,10 +30,6 @@ if (cluster.isMaster) {
     console.log(`Handling request from ${pid}`);
     res.end(`Hello from ${pid}\n`);
   });
-  setTimeout(
-    () => { throw new Error('Ooops') },
-    Math.ceil(Math.random() * 3) * 1000
-  )
   server.listen(8080, () => console.log(`Started at ${pid}`));
 }
 // Each worker is a different Node.js process with its own event loop, memory space, and loaded modules.
