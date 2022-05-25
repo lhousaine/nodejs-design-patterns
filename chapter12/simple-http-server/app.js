34a392dfc7b83fe79f432c1c5a4d62d2c5c79aad
lhousaine <l.ouarhou.ensa@gmail.com>
2022-05-25 20:14:25 +0100

    feat(chapter12):simulate restarting one process after crashing and calculate availability

--- chapter12/simple-http-server/app.js
+++ chapter12/simple-http-server/app.js
@@ -7,6 +7,14 @@ if (cluster.isMaster) {
   const availableCpus = cpus();
   console.log(`Clustering to ${availableCpus.length} processes`);
   availableCpus.forEach(() => cluster.fork());
+  cluster.on("exit", (worker, code) => {
+    if (code !== 0 && !worker.exitedAfterDisconnect) {
+      console.log(
+        `Worker ${worker.process.pid} crashed. ` + "Starting a new worker"
+      );
+      cluster.fork();
+    }
+  });
 } else {
   // do Work
   const { pid } = process;
@@ -18,6 +26,10 @@ if (cluster.isMaster) {
     console.log(`Handling request from ${pid}`);
     res.end(`Hello from ${pid}\n`);
   });
+  setTimeout(
+    () => { throw new Error('Ooops') },
+    Math.ceil(Math.random() * 3) * 1000
+  )
   server.listen(8080, () => console.log(`Started at ${pid}`));
 }
 // Each worker is a different Node.js process with its own event loop, memory space, and loaded modules.
