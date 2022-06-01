ff6f40b9763ba58c0b6f3f5bc42bae5713bc2b7b
lhousaine <l.ouarhou.ensa@gmail.com>
2022-06-01 22:42:48 +0100

    feat(chapter13):add publisher/subscriber example with webSocket

---
+++ chapter13/real-time-chat-app/index.js
@@ -0,0 +1,28 @@
+import { createServer } from "http";
+import staticHandler from "serve-handler";
+import webSocket, {WebSocketServer} from "ws";
+
+// serve static files
+const server = createServer((req, res) => {
+  return staticHandler(req, res, { public: "www" });
+});
+// (1) create an http server and forward every request to to a special handler, which will take care to serve all the static files from the www directory.
+
+const wss = new WebSocketServer({ server }); // create a ws and attach it to our http server.
+wss.on("connection", (client) => { // listen for incoming websockets client connections.
+  console.log("Client connected");
+  client.on("message", (msg) => { // listen for incoming messages
+    console.log(`Message: ${msg}`);
+    broadcast(msg); // broadcast the message to all connected clients.
+  });
+}); // (2)
+
+function broadcast(msg) {
+  for (const client of wss.clients) {
+    if (client.readyState === webSocket.OPEN) {
+      client.send(msg);
+    }
+  }
+}
+
+server.listen(process.argv[2] || 8080);
