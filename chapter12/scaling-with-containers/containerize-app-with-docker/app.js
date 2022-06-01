e1ef27de676f95b17a1651efc994e81de166664e
lhousaine <l.ouarhou.ensa@gmail.com>
2022-06-01 22:41:52 +0100

    feat(chapter12):add scaling application with containers example

---
+++ chapter12/scaling-with-containers/containerize-app-with-docker/app.js
@@ -0,0 +1,14 @@
+import { createServer } from "http";
+import { hostname } from "os";
+
+const version = 3;
+
+const server = createServer((req, res) => {
+  let i = 1e7;
+  while (i > 0) {
+    i--;
+  }
+  res.end(`Hello from ${hostname()} (v${version})`);
+});
+
+server.listen(8080);
